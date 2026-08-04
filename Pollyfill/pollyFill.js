a8e1799509059a9032214e1e09927e95ac38a398
ketanprajapati005 <146518990+ketanprajapati005@users.noreply.github.com>
2026-08-04 19:24:06 +0530

    reduse function using pollyfill

--- Pollyfill/pollyFill.js
+++ Pollyfill/pollyFill.js
@@ -33,26 +33,50 @@
 
 
 // <--- pollyfill of Filter function --->
+// let arr = [1,2,3,4,5];
+
+// function cb(ele){
+//     return ele%2==0;
+// };
+
+// Array.prototype.customFilter = function (cb){
+//     let inputArray = this;
+//     let ans = [];
+
+//     for(let i=0; i<inputArray.length; i++){
+//         let res = cb(inputArray[i], i, inputArray);
+        
+//         if(res){
+//             ans.push(inputArray[i]);
+//         }
+
+//     }
+//     return ans;
+// }
+
+// let ans = arr.customFilter(cb);
+// console.log(ans);
+
 let arr = [1,2,3,4,5];
 
-function cb(ele){
-    return ele%2==0;
+let cb =function (acc,currEle){
+    return acc + currEle;
 };
 
-Array.prototype.customFilter = function (cb){
+Array.prototype.customFilter = function (cb, defaultValue){
     let inputArray = this;
-    let ans = [];
+    let ans = null;
 
-    for(let i=0; i<inputArray.length; i++){
-        let res = cb(inputArray[i], i, inputArray);
-        
-        if(res){
-            ans.push(inputArray[i]);
-        }
-
+    let index = 0;
+    let acc = defaultValue;
+    if(!defaultValue){
+        index = 1;
+        acc = inputArray[0];
     }
-    return ans;
-}
 
-let ans = arr.customFilter(cb);
-console.log(ans);
+    while(index<inputArray.length){
+        acc = cb(acc, inputArray[index]);
+        index++;
+    }    
+    return acc;
+}    
